bcb58fe964cb1f05be73a067a8950e38bce9b631
sam0sva1 <konstantus@live.ru>
2017-02-05 21:28:39 +0300

    RR fixs syntaxical bug and adds getter state

--- reducer.js
+++ reducer.js
@@ -1,5 +1,3 @@
-let state = 0;
-
 function updateState(state, action) {
 	if (action.type === 'INCREMENT') {
 		return state + action.amount;
@@ -10,10 +8,14 @@ function updateState(state, action) {
 	}
 }
 
-class Store(updateState, state) {
-	constructor() {
+class Store {
+	constructor(updateState, state) {
 		this._state = state;
 		this._updateState = updateState;
+	}
+
+	get state() {
+		return this._state;
 	}
 
 	update(action) {
@@ -27,11 +29,11 @@ let store = new Store(updateState, 0);
 const incrementAction = { type: 'INCREMENT', amount: 5 };
 const decrementAction = { type: 'DECREMENT', amount: 3 };
 
-state = updateState(state, incrementAction);
-console.log(state);
+store.update(incrementAction);
+console.log(store.state);
 
-state = updateState(state, decrementAction);
-console.log(state);
+store.update(decrementAction);
+console.log(store.state);
 
-state = updateState(state, {});
-console.log(state);
+store.update({});
+console.log(store.state);
